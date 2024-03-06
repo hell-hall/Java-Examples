7a59ba3aed01498acf9cc8e8638071c74aa9271c
hell-hall <90412878+hell-hall@users.noreply.github.com>
2024-03-05 22:59:28 -0600

    Add files via upload

---
+++ returning a number when a return function is called/src/App.java
@@ -0,0 +1,23 @@
+public class App {
+
+    public static void main(String[] args) 
+    {
+        /* Functions that return a value can be used
+        like any other native data type like (int)
+        for example instead of (inta + intb) it would be
+        (getIntA + getIntB)
+         */
+        System.out.println(getInt());
+    }
+    // How to use return statment or functions
+    // This will be a exmple of returning a number
+
+    public static int getInt() 
+    {
+        // This is what is returned when getInt is called
+        /* return is the keyword for returning data after 
+        a function is called */
+        return 7;
+    }
+
+}
