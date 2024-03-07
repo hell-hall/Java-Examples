78490261972741ba85f784f691fb90ac4609c02c
hell-hall <90412878+hell-hall@users.noreply.github.com>
2024-03-07 02:34:39 -0600

    Add files via upload

---
+++ How to use if statments in java/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+
+    public static void main(String[] args) 
+    {
+        // we assign number to eqaul 5
+        // int is whole numbers in java
+        int number = 6;
+        /* A simple if statement that checks to see if number 
+        equals five and if not execute the code in else
+        */
+        if (number == 5) {
+            System.out.println("Equals five");
+        } else {
+            System.out.println("Does not equal 5");
+        }
+    }
+}
